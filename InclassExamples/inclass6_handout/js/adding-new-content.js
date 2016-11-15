93f8f07da8dbe417bf52e64e963b63aa5363ebdd
Jonathan Wydola <Jonathan Wydola>
2016-11-15 17:17:37 -0500

    added a ) in the adding-new-content.js file

--- InclassExamples/inclass6_handout/js/adding-new-content.js
+++ InclassExamples/inclass6_handout/js/adding-new-content.js
@@ -3,7 +3,7 @@ $(function() {
         return '<p>Just Updated</p>';
     });
     $('li.hot').prepend('+');
-    var newValANC = $('<li><em>gluten free</em> soy sauce</li>';
+    var newValANC = $('<li><em>gluten free</em> soy sauce</li>');
     $('li').last().after(function () {
         return newValANC;
     });
